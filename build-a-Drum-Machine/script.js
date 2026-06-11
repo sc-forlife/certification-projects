4f9c80cbf32be8faaa9f7645b160643f102027e6
sc-forlife <salemsc7@gmail.com>
2026-06-11 11:11:02 +0200

    Assigning sound to each later and click

--- build-a-Drum-Machine/script.js
+++ build-a-Drum-Machine/script.js
@@ -1,18 +1,30 @@
-const heater1 = document.getElementById("Q");
-const drumPadQ = document.getElementById("Heater-1");
 const display = document.getElementById("display");
 const padBank = document.getElementById("pad-bank");
 
-function onPlay(audio, id) {
+function onPlay(audio) {
   audio.play();
 
-  display.textContent = id;
+  display.textContent = audio.title;
 
   audio.addEventListener("ended", () => {
     display.textContent = "";
   });
 }
 
+function keyDownPlay(key) {
+  if (!document.getElementById(key.toUpperCase())) {
+    return;
+  }
+
+  const sound = document.getElementById(key.toUpperCase());
+
+  onPlay(sound);
+}
+
 padBank.addEventListener("click", (e) => {
-  onPlay(e.target.childNodes[1], e.target.id);
+  onPlay(e.target.childNodes[1], e.target.getAttribute("aria-label"));
+});
+
+document.addEventListener("keydown", (e) => {
+  keyDownPlay(e.key);
 });
