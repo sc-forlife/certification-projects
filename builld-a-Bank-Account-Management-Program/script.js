fbd4718a48c947ac7dc690a2f1b1e8b4d5eefc1b
sc-forlife <salemsc7@gmail.com>
2026-08-04 06:07:42 +0200

    Completed the Bank Account Management Program

--- builld-a-Bank-Account-Management-Program/script.js
+++ builld-a-Bank-Account-Management-Program/script.js
@@ -1,44 +1,43 @@
-export class BankAcoount {
+export class BankAccount {
   constructor() {
     this.balance = 0;
-    this.transaction = [];
-    // transaction = {type:withdraw/deposit , amount:int}
+    this.transactions = [];
   }
   deposit(depositAmount) {
     if (depositAmount <= 0) {
       return "Deposit amount must be greater than zero.";
     }
-    this.transaction.push({ type: deposit, amount: depositAmount });
+    this.transactions.push({ type: "deposit", amount: depositAmount });
     this.balance += depositAmount;
-    return `Successfully deposited ${depositAmount}. New balance: ${this.balance}`;
+    return `Successfully deposited $${depositAmount}. New balance: $${this.balance}`;
   }
   withdraw(withdrawAmount) {
     if (withdrawAmount > 0 && withdrawAmount <= this.balance) {
-      this.transaction.push({ type: withdraw, amount: withdrawAmount });
+      this.transactions.push({ type: "withdraw", amount: withdrawAmount });
       this.balance -= withdrawAmount;
-      return `Successfully withdrew ${withdrawAmount}. New balance: ${this.balance}`;
+      return `Successfully withdrew $${withdrawAmount}. New balance: $${this.balance}`;
     } else if (withdrawAmount <= 0 || withdrawAmount > this.balance) {
       return `Insufficient balance or invalid amount.`;
     }
   }
   checkBalance() {
-    return `Current balance: ${balance}`;
+    return `Current balance: $${this.balance}`;
   }
   listAllDeposits() {
-    const depositTransactions = this.balance.filter(
+    const depositTransactions = this.transactions.filter(
       (transaction) => transaction.type === "deposit",
     );
     return `Deposits: ${depositTransactions.map((transaction) => `${transaction.amount}`)}`;
   }
   listAllWithdrawals() {
-    const withdrawTransactions = this.balance.filter(
+    const withdrawTransactions = this.transactions.filter(
       (transaction) => transaction.type === "withdraw",
     );
-    return `Deposits: ${withdrawTransactions.map((transaction) => `${transaction.amount}`)}`;
+    return `Withdrawals: ${withdrawTransactions.map((transaction) => `${transaction.amount}`)}`;
   }
 }
 
-const myAccount = new BankAcoount();
+const myAccount = new BankAccount();
 myAccount.deposit(50);
 myAccount.deposit(150);
 myAccount.withdraw(10);
--- builld-a-Bank-Account-Management-Program/script.test.js
+++ builld-a-Bank-Account-Management-Program/script.test.js
@@ -1,9 +1,9 @@
 import { expect, it, describe } from "vitest";
-import { BankAcoount } from "./script";
+import { BankAccount } from "./script";
 
-desciribe("Bank Account", () => {
-  it("Deposit Amount", () => {
-    const myBank = new BankAcoount();
+describe("Bank Account", () => {
+  it("Deposit Amount and withdrawls", () => {
+    const myBank = new BankAccount();
     expect(myBank.withdraw(-50)).toEqual(
       "Insufficient balance or invalid amount.",
     );
@@ -26,8 +26,23 @@ desciribe("Bank Account", () => {
       "Successfully deposited $100. New balance: $200",
     );
     expect(myBank.withdraw(200)).toEqual(
-      "Successfully withdrew $150. New balance: $50",
+      "Successfully withdrew $200. New balance: $0",
     );
-    expect(myBank.checkBalance()).toEqual("Current balance: $200");
+    expect(myBank.checkBalance()).toEqual("Current balance: $0");
+  });
+  it("ListAllDeposits", () => {
+    const myBank = new BankAccount();
+    myBank.deposit(10);
+    myBank.deposit(35);
+    myBank.deposit(90);
+    expect(myBank.listAllDeposits()).toEqual("Deposits: 10,35,90");
+  });
+  it("ListAllWithdrawls", () => {
+    const myBank = new BankAccount();
+    myBank.deposit(170);
+    myBank.withdraw(20);
+    myBank.withdraw(50);
+    myBank.withdraw(100);
+    expect(myBank.listAllWithdrawals()).toEqual("Withdrawals: 20,50,100");
   });
 });
